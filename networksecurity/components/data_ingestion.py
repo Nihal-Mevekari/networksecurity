b56c270c19884f9d01be660ae88a71911e7e8e31
Nihal Mevekari <nihalmevekari@gmail.com>
2025-02-02 01:11:01 +0530

    Data Validation completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -11,7 +11,6 @@ import sys
 import numpy as np
 import pandas as pd
 import pymongo
-import bson
 from typing import List
 from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
