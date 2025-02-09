55137ece3e25c5abb8bef785b07edc4b05c46f24
Nihal Mevekari <nihalmevekari@gmail.com>
2025-02-09 16:15:00 +0530

    Cloud S3 Data Storage

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -24,17 +24,17 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
-'''
+
 from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
 from networksecurity.cloud.s3_syncer import S3Sync
 from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
 import sys
-'''
+
 
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config=TrainingPipelineConfig()
-        #self.s3_sync = S3Sync()
+        self.s3_sync = S3Sync()
         
 
     def start_data_ingestion(self):
@@ -88,7 +88,7 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-    '''
+    
     ## local artifact is going to s3 bucket    
     def sync_artifact_dir_to_s3(self):
         try:
@@ -105,7 +105,7 @@ class TrainingPipeline:
             self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    ''' 
+    
     
     
     def run_pipeline(self):
@@ -115,8 +115,8 @@ class TrainingPipeline:
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
-            #self.sync_artifact_dir_to_s3()
-            #self.sync_saved_model_dir_to_s3()
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             
             return model_trainer_artifact
         except Exception as e:
